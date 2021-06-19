6c3d398e1977511d5d711dce4f0eff6292931282
mmanso <mayara.manso@mercadolivre.com>
2021-06-18 22:51:27 -0300

    creates config db

---
+++ src/config/mongodb.config.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to database!'))
+  .catch(error => console.log(`error to connect database`, { error }));
