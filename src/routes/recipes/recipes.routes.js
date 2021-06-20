3a05d34c89cf09cc8514707ee2a8121d12453bf2
mmanso <mayara.manso@mercadolivre.com>
2021-06-20 02:00:37 -0300

    creates recipes route

---
+++ src/routes/recipes/recipes.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const RecipesController = require('../../controller/recipes.controller');
+const router = express();
+
+
+router.post('/recipe', RecipesController.create);
+router.patch('/recipe/:recipeId', RecipesController.update);
+
+router.get('/recipes', RecipesController.getAll);
+router.get('/recipes/name/:name', RecipesController.getByName);
+router.get('/recipes/search', RecipesController.search);
+
+module.exports = router;
