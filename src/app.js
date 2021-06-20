8a7cbc268c86732584de6baec17e18e9879d4ede
mmanso <mayara.manso@mercadolivre.com>
2021-06-20 02:00:51 -0300

    import recipes router in app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 require('dotenv').config()
 const express = require('express');
+
+const recipesRouter = require('./routes/recipes/recipes.routes');
+
 require('./config/mongodb.config');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const prefix = 'cook-notes-api';
+
+app.use(`/${prefix}`, recipesRouter);
 
 app.listen(process.env.PORT, () => console.log(`App listen on PORT ${process.env.PORT}`));
