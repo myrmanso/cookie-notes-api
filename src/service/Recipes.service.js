2615097df3fe54a85d6c0bc2b22754776bdd2bb9
mmanso <mayara.manso@mercadolivre.com>
2021-06-20 02:03:44 -0300

    add method searchRecipeByName and searchRecipe

--- src/service/Recipes.service.js
+++ src/service/Recipes.service.js
@@ -5,6 +5,23 @@ class RecipesService extends BaseService {
   constructor() {
     super(Recipes)
   }
+
+  searchRecipeByName = async (nameRecipe) => {
+    try {
+      return this.model.find({ name: { "$regex": `${nameRecipe}`, "$options": "i" } }, 'name')
+    } catch (error) {
+      console.log('RecipesService.searchRecipeName - error ', error)
+    }
+  }
+
+  searchRecipe = async (objectSearch) => {
+    try {
+      return await this.model.find(objectSearch)
+    } catch (error) {
+      console.log('RecipesService.searchRecipeName - error ', error)
+    }
+  }
+
 }
 
 module.exports = new RecipesService();
