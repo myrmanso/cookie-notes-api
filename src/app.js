ff2eb060768fd02c72f473ccef8dbf28a7e9a2ae
mmanso <mayara.manso@mercadolivre.com>
2021-07-01 00:42:56 -0300

    config cors, create prefix and import auth routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
 
+const authRouter = require('./routes/auth/auth.routes');
+const userRouter = require('./routes/user/user.routes');
 const recipesRouter = require('./routes/recipes/recipes.routes');
 
 require('./config/mongodb.config');
 
 const app = express();
 
+app.use(cors({
+  origin: process.env.FRONT_END_URL,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const prefix = 'cook-notes-api';
+const prefix = 'api';
 
+app.use(`/${prefix}/auth`, authRouter);
+app.use(`/${prefix}/user`, userRouter);
 app.use(`/${prefix}`, recipesRouter);
 
 app.listen(process.env.PORT, () => console.log(`App listen on PORT ${process.env.PORT}`));
