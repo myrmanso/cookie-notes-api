8055f56ae83cd7aca524e1acf434f55e4f9c3a9d
mmanso <mayara.manso@mercadolivre.com>
2021-07-01 00:41:58 -0300

    add unique to email

--- src/models/Users.model.js
+++ src/models/Users.model.js
@@ -2,7 +2,7 @@ const { Schema, model, Types } = require('mongoose');
 
 const userSchema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
   active: { type: Boolean, default: true },
