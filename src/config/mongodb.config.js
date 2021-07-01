cd20febf49c5ebd272073851abd0e36f473595ae
mmanso <mayara.manso@mercadolivre.com>
2021-07-01 00:36:58 -0300

    update config with useCreateIndex

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => console.log('Connected to database!'))
   .catch(error => console.log(`error to connect database`, { error }));
